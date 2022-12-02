56372a0719c49d82da4e1a6360f6c19281b54934
flaviamiranda <flaviagracianom@gmail.com>
2022-12-02 11:29:38 -0300

    setup the basket, able to add items, created a delete item button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,16 +3,21 @@ import Header from '../Header/Header';
 import Products from '../Products/Products';
 import Profile from '../Profile/Profile';
 import Basket from '../Basket/Basket';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [basket, setBasket] = useState([])
+
+
   return (
     <div className="App">
       <Header />
       <main>
-        <Products />
+        <Products basket={basket} setBasket={setBasket} />
         <div className='sidebar'>
           <Profile />
-          <Basket />
+          <Basket basket={basket} />
         </div>
       </main>
     </div>
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,20 +1,30 @@
-import React from 'react'
-import './Item.css'
+// import React, { useEffect } from "react";
+import "./Item.css";
 
-export default function Item({product}) {
+export default function Item({ product, basket, setBasket }) {
+
+  // useEffect(() => {
+    async function addToBasket() {
+      await setBasket([...basket, product]);
+      console.log(basket);
+    }
+    // addToBasket();
+  // }, []);
 
   return (
-    <div className='Item'>
-      <div className='item-image'>
+    <div className="Item">
+      <div className="item-image">
         <img src={product.image} alt={product.title}></img>
       </div>
-      <div className='product-details'>
+      <div className="product-details">
         <h3>{product.title.substring(0, 60)}...</h3>
         <h3>£{product.price}</h3>
         <p>{product.description.substring(0, 100)}...</p>
-        <button className='view-item-button'>View Item</button>
-        <button className='add-to-basket-button'>Add To Basket</button>
+        <button className="view-item-button">View Item</button>
+        <button onClick={addToBasket} className="add-to-basket-button">
+          Add To Basket
+        </button>
       </div>
     </div>
-  )
+  );
 }
